5c5b2bda2e170ad5bf913b0346555e08ce353fc2
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2025-06-03 20:12:06 -0600

    changed to access to ['my_list']

--- Chapter11/Section1/section1.py
+++ Chapter11/Section1/section1.py
@@ -4,7 +4,7 @@ if 'my_key'  not in st.session_state:
     st.session_state['my_key']="value"
     
 if 'my_list' not in st.session_state:
-    st.session_state.my_list = []
+    st.session_state['my_list'] = []
 
 st.text_input("Your name", key="name")
 st.slider('Select a number',key='number')
