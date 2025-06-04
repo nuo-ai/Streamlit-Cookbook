fd9eaa206a9fd36049d93e46932d44163cbbfb59
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2025-06-04 12:25:37 -0600

    Changed the recipe

--- Chapter11/Section4/section4.py
+++ Chapter11/Section4/section4.py
@@ -1,13 +1,17 @@
 import streamlit as st
 
-if "counter" not in st.session_state:
-    st.session_state.counter = 0
 
-def add_number(num):
-    st.session_state.counter += num
+if "interaction_count" not in st.session_state:
+    st.session_state.interaction_count = 0
 
-number = st.number_input("Number to add:",step=1)
+def count_interaction():
+    st.session_state.interaction_count += 1
 
-button = st.button("Add number", on_click=lambda: add_number(number))
 
-st.write(st.session_state.counter)
+st.text_input("Enter your name", key="name", on_change=count_interaction)
+st.slider("Pick a number", 0, 10, key="number", on_change=count_interaction)
+st.radio("Choose an option", ["A", "B", "C"], key="choice", on_change=count_interaction)
+st.button("Click me", on_click=count_interaction)
+
+st.markdown("### 🔢 Total interactions:")
+st.write(st.session_state.interaction_count)
